6e3431dd8c45bd244a1ab84349ad24e3caf01ddb
Sabya <sabyasachi.exe@gmail.com>
2023-02-07 14:54:19 -0800

    Adding header block and fetching questions data

--- libs/blocks/quiz/quiz.js
+++ libs/blocks/quiz/quiz.js
@@ -1,18 +1,70 @@
-import { html, signal, useEffect } from "../../deps/htm-preact.js";
+import { html, signal, useEffect } from '../../deps/htm-preact.js';
 // import quizcontainer from './quizcontainer.js'
 
 const data = signal({});
+const metrics = {
+  prevQuestion : null,
+  prevAnswer : null,
+  currentQuestion: null,
+  currentAnswer: null,
+  nextQuestion: null,
+  nextAnswer: null
+}
+
+export const userJourney = signal(metrics) 
+
 
 export default function init(el) {
   // const app = html` <${quizcontainer} /> `;
   // render(app, el);
   const link = el.querySelector(':scope > div > div > a');
-  console.log(link.text) // This is the url for the questions.
-  fetchData(el, link)
+  console.log(link.text); // This is the url for the questions.
+  fetchQuestionsData(el, link);
 }
 
+// Get initial quiz data
+async function fetchQuestionsData(el, link) {
+  const resp = await fetch(link.text.toLowerCase());
+
+  if (!resp.ok) return {};
+
+  const json = await resp.json();
+  console.log(json);
+  collectQuestions(el, json);
+  return json;
+}
+
+
+// Gather all questions
+const collectQuestions = (el, response) => {
+  console.log('collecting questions');
+  console.log(response.questions.data);
+
+  // If there is no questions, the data is malformed.
+  if (!response.questions) {
+    console.log('Malformed data. Questions must be defined')
+  }
+
+  const questions = response.questions.data;
+  console.log('user journey', userJourney.value);
+  // Lets set the first question as current question.
+  // Previous and next question will be null.
+  userJourney.value.currentQuestion = response.questions.data[0];
+  console.log('user journey', userJourney.value);
+
+  el.classList.add('quiz-container');
+  const children = el.querySelectorAll(':scope > div');
+  if (children.length > 0) {
+    children[0].classList.add('background');
+    decorateBlockBg(children[0]);
+  }
+
+  decorateBlockForeground(el);
+};
+
+
 const decorateBlockBg = (node) => {
-  console.log('decorating bg', node)
+  console.log('decorating bg', node);
   // node.classList.add('background');
   const backgroundElemTpl = `<div data-valign="middle">
   <picture>
@@ -21,68 +73,31 @@ const decorateBlockBg = (node) => {
     <source type="image/png" srcset="./media_1b2111891fc6836011dc3d527c6999c59e792ed70.png?width=2000&amp;format=png&amp;optimize=medium" media="(min-width: 400px)">
     <img loading="eager" alt="" type="image/png" src="./media_1b2111891fc6836011dc3d527c6999c59e792ed70.png?width=750&amp;format=png&amp;optimize=medium" width="750" height="375">
   </picture>
-</div>`
+</div>`;
 
-node.innerHTML = backgroundElemTpl // Adding the background image for the quiz block.
+  node.innerHTML = backgroundElemTpl; // Adding the background image for the quiz block.
 };
 
 const decorateBlockForeground = (el) => {
-  console.log('decorating foreground')
+  console.log('decorating foreground');
   // node.classList.add('foreground');
   const foregroundElemTpl = `<div class="foreground container">
   <div data-valign="middle" class="text" daa-lh="What do you want to do today?">
   <h2 id="heading-xl-marquee-standard-medium-left" class="heading-XL">
   What do you want to do today?
   </h2>
-    <p class="detail-M">Pick up to three.</p>
-
-    <div class="milo-card-wrapper consonant-Wrapper consonant-Wrapper--1200MaxWidth"><div class="consonant-Wrapper-inner"><div class="consonant-Wrapper-collection"><div class="consonant-CardsGrid consonant-CardsGrid--3up consonant-CardsGrid--with4xGutter"><div class="card product-card border consonant-Card consonant-ProductCard">
-          
-        <div class="consonant-ProductCard-inner">
-            <div data-valign="middle" class="consonant-ProductCard-row">
-              
-              <h3 id="lorem-ipsum-dolor-sit-amet" class="consonant-ProductCard-title">Lorem ipsum dolor sit amet</h3>
-              
-              
-            </div>
-          <p class="consonant-ProductCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p><div class="consonant-CardFooter"><div class="consonant-CardFooter-row" data-cells="2"><div class="consonant-CardFooter-cell consonant-CardFooter-cell--left"><a href="https://business.adobe.com/" class="con-button outline">Learn more</a></div><div class="consonant-CardFooter-cell consonant-CardFooter-cell--right"><a href="https://business.adobe.com/" class="con-button blue">Sign up</a></div></div></div></div></div></div></div></div></div>
-    <p class="action-area"><a href="https://www.adobe.com/" class="con-button outline button-L button-justified-mobile" daa-ll="outline|Lorem ipsum 1">Lorem ipsum</a> <a href="https://www.adobe.com/" class="con-button blue button-L button-justified-mobile" daa-ll="filled|Learn more 2">Learn more</a> <a href="https://www.adobe.com/" daa-ll="link|Text link 3">Text link</a></p>
+    <p class="detail-L">Pick up to three.</p>
   </div>
-</div>`
+</div>`;
 
-el.insertAdjacentHTML('beforeend', foregroundElemTpl) // Adding foreground elements.
-
+console.log('quiz option', quizOption)
+  el.insertAdjacentHTML('beforeend', foregroundElemTpl); // Adding foreground elements.
+  el.insertAdjacentHTML('beforeend', quizOption); // Adding quiz options.
 };
 
-
-// Get initial quiz data
-async function fetchData(el, link) {
-  const resp = await fetch(link.text.toLowerCase());
-
-  if (!resp.ok) return {};
-
-  const json = await resp.json();
-  console.log(json)
-  collectQuestions(el, json);
-  return json;
-}
-
-// Gather all questions
-const collectQuestions = (el, response) => {
-  console.log('collecting questions');
-  console.log(response.questions);
-  el.classList.add('quiz-container')
-  const children = el.querySelectorAll(':scope > div');
-  if (children.length > 0) {
-    children[0].classList.add('background');
-    decorateBlockBg(children[0]);
-  }
-
-  decorateBlockForeground(el);
-}
-
-
-
+export const quizOption = `<div class="section 4-up">
+Cards
+    </div>`
 
 
 
