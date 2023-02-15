dff72bfdbf8d35b8f210503df8e7023e734de061
Sabya <sabyasachi.exe@gmail.com>
2023-02-14 18:09:01 -0800

    Adding dynbamic data for questions and options

--- libs/blocks/quiz/quiz.js
+++ libs/blocks/quiz/quiz.js
@@ -13,6 +13,11 @@ const metrics = {
   nextAnswer: null,
 };
 
+let questionsData = {}
+let stringsData = {}
+let resultsData = {}
+
+
 const QUESTIONS_EP_NAME = 'questions.json';
 const STRINGS_EP_NAME = 'strings.json';
 const RESULTS_EP_NAME = 'results.json';
@@ -21,52 +26,39 @@ export const userJourney = signal(metrics);
 
 export default function init(el) {
   const link = el.querySelector(':scope > div > div > a');
-  const quizConfigPath = link.text.toLowerCase();
-  fetchQuestionsData(el, quizConfigPath);
-  // fetchStringsData(el, quizConfigPath); // Call Strings data only after the questions data loads.
+  window.milo = window.milo || {}
+  window.milo.quizConfigPath = link.text.toLowerCase();
+  fetchQuestionsData(el);
 }
 
 // Get initial quiz data
-async function fetchQuestionsData(el, quizConfigPath) {
-  await fetch(quizConfigPath + QUESTIONS_EP_NAME)
+async function fetchQuestionsData(el) {
+  await fetch(window.milo.quizConfigPath + QUESTIONS_EP_NAME)
     .then((resp) => {
       if (!resp.ok) return {};
       const json = resp.json();
       collectQuestions(el, json);
-      // return json;
     })
     .then(() => {
-      // fetch strings data only after the question data is loaded.
-      fetchStringsData(el, quizConfigPath);
+      // Do nothing
     });
 }
 
 // Get initial quiz data
-async function fetchStringsData(el, quizConfigPath) {
-  fetch(quizConfigPath + STRINGS_EP_NAME).then((stringsResponse) => {
+async function fetchStringsData(el) {
+  fetch(window.milo.quizConfigPath + STRINGS_EP_NAME).then((stringsResponse) => {
     // if (!stringsResponse.ok) return {};
     const stringsData = stringsResponse.json();
-    // console.log('strings data', stringsData);
     collectStrings(el, stringsData);
   });
-
-  // return stringsData;
 }
 
 const collectStrings = (el, stringsData) => {
   stringsData.then((data) => {
-    console.log('chompa', data)
-    const object = data.questions.data.find(
-      (o) => o.q === userJourney.value.currentQuestion.questions
-    );
-    userJourney.value.currentQuestion = object
-    console.log('user journey in collecting strings', userJourney.value.currentQuestion.heading);
+    stringsData = data;
+    const object = data.questions.data.find((o) => o.q === userJourney.value.currentQuestion.questions);
+    decorateBlockForeground(el, object)
   })
-  
-  
-  // userJourney.value.currentQuestion.heading = stringsData
-  // userJourney.value.currentQuestion.subhead =
-  // userJourney.value.currentQuestion.btn =
 };
 
 // Gather all questions
@@ -75,7 +67,7 @@ const collectQuestions = (el, response) => {
   // console.log('response in collectQuestions', typeof response);
 
   response.then((resp) => {
-    console.log('promise is ', resp);
+    questionsData = resp;
     if (!resp.questions) {
       console.log('Malformed data. Questions must be defined');
     }
@@ -84,8 +76,8 @@ const collectQuestions = (el, response) => {
     // Lets set the first question as current question.
     // Previous and next question will be null.
     userJourney.value.currentQuestion = resp.questions.data[0];
-    // console.log('Current question is', response.questions);
-    // console.log('user journey', userJourney.value.currentQuestion);
+    console.log('Current question is', resp.questions);
+    console.log('user journey after collecting the first question: ', userJourney.value.currentQuestion);
 
     el.classList.add('quiz-container');
     const children = el.querySelectorAll(':scope > div');
@@ -93,75 +85,133 @@ const collectQuestions = (el, response) => {
       children[0].classList.add('background');
       decorateBlockBg(children[0]);
     }
-
-    decorateBlockForeground(el);
+    fetchStringsData(el);
   });
 };
 
 const decorateBlockBg = (node) => {
   const backgroundElemTpl = `<div data-valign="middle">
   <picture>
-    <source type="image/webp" srcset="./media_1b2111891fc6836011dc3d527c6999c59e792ed70.png?width=2000&amp;format=webply&amp;optimize=medium" media="(min-width: 400px)">
-    <source type="image/webp" srcset="./media_1b2111891fc6836011dc3d527c6999c59e792ed70.png?width=750&amp;format=webply&amp;optimize=medium">
-    <source type="image/png" srcset="./media_1b2111891fc6836011dc3d527c6999c59e792ed70.png?width=2000&amp;format=png&amp;optimize=medium" media="(min-width: 400px)">
-    <img loading="eager" alt="" type="image/png" src="./media_1b2111891fc6836011dc3d527c6999c59e792ed70.png?width=750&amp;format=png&amp;optimize=medium" width="750" height="375">
+    <source type="image/webp" srcset="${userJourney.value.currentQuestion.background}" media="(min-width: 400px)">
+    <source type="image/webp" srcset="${userJourney.value.currentQuestion.background}" >
+    <source type="image/png" srcset="${userJourney.value.currentQuestion.background}"  media="(min-width: 400px)">
+    <img loading="eager" alt="" type="image/png" src="${userJourney.value.currentQuestion.background}"  width="750" height="375">
   </picture>
 </div>`;
 
   node.innerHTML = backgroundElemTpl; // Adding the background image for the quiz block.
 };
 
-const decorateBlockForeground = (el) => {
+const decorateBlockForeground = (el, object) => {
   const foregroundElemTpl = `<div class="foreground container">
-  <div data-valign="middle" class="text" daa-lh=${userJourney.value.currentQuestion.questions}>
+  <div data-valign="middle" class="text" daa-lh=${object.heading}>
     <h2 id="heading-xl-marquee-standard-medium-left" class="heading-XL">
-    ${console.log('in html', userJourney.value.currentQuestion.questions)}
-    
+    ${object.heading}
     </h2>
-    <!-- <p class="detail-L">${userJourney.value.currentQuestion.subhead}</p> -->
-    Pick up to three. 
+    <p class="detail-L">${object.subhead}</p>
   </div>
 </div>`;
   el.insertAdjacentHTML('beforeend', foregroundElemTpl); // Adding foreground elements.
-  el.insertAdjacentHTML('beforeend', quizOption); // Adding quiz options.
+
+  // Lets create the options.
+  // Find optioons for the current questions.
+  findAndInsertOptions(el);
+
+  // console.log('user journey after foreground decor is done: ', userJourney.value)
 };
 
-export const quizOption = `
-<div class="milo-card-wrapper consonant-Wrapper consonant-Wrapper--1200MaxWidth"><div class="consonant-Wrapper-inner"><div class="consonant-Wrapper-collection"><div class="consonant-CardsGrid consonant-CardsGrid--4up consonant-CardsGrid--with4xGutter"><div class="card half-card border consonant-Card consonant-OneHalfCard">
-          
-        <div class="consonant-OneHalfCard-img" style="background-image: url(&quot;https://main--milo--adobecom.hlx.page/drafts/rclayton/media_138263efad88faf6be71169bc1fc66ee2dfb96661.jpeg?width=750&amp;format=jpeg&amp;optimize=medium&quot;);"></div><div class="consonant-OneHalfCard-inner">
-            <div data-valign="middle">
-              
-              <h3 id="lorem-ipsum-dolor-sit-amet" class="consonant-OneHalfCard-title">Lorem ipsum dolor sit amet</h3>
-              <p class="consonant-OneHalfCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
+const findAndInsertOptions = (el) => {
+  
+  console.log('questionsData : ', JSON.parse(JSON.stringify(questionsData)));
+  const hamaraData = JSON.parse(JSON.stringify(questionsData));
+
+  console.log('hamaradata : ' , hamaraData);
+  const currQuestion = userJourney.value.currentQuestion.questions;
+  const currentQuestionOptions = hamaraData[currQuestion].data;
+  console.log('currentQuestionOptions data' , currentQuestionOptions)
+  
+
+  el.insertAdjacentHTML('beforeend', quizOption); // Adding quiz options.
+}
+
+
+const createOption = (optionsData) => {
+  optionsData.map((data) => {
+
+  })
+}
+
+
+export const quizOption = (currentQuestionOptions) => { 
+  
+  return `
+<div class="milo-card-wrapper consonant-Wrapper consonant-Wrapper--1200MaxWidth">
+  <div class="consonant-Wrapper-inner">
+    <div class="consonant-Wrapper-collection">
+      <div class="consonant-CardsGrid consonant-CardsGrid--5up consonant-CardsGrid--with4xGutter">
+
+
+        // TODO: Need to make this dynamic.
+        ${currentQuestionOptions.map(option => html`<div class="Chintu">Arshad Miyan</div>`)}
+
+          <div class="card half-card border consonant-Card consonant-OneHalfCard">
+            <div class="consonant-OneHalfCard-img" style="background-image: url(&quot;https://main--milo--adobecom.hlx.page/drafts/rclayton/media_138263efad88faf6be71169bc1fc66ee2dfb96661.jpeg?width=750&amp;format=jpeg&amp;optimize=medium&quot;);">
+            </div>
+            <div class="consonant-OneHalfCard-inner">
+              <div data-valign="middle">  
+                <h3 id="lorem-ipsum-dolor-sit-amet-3" class="consonant-OneHalfCard-title">Lorem ipsum dolor sit amet</h3>
+                <p class="consonant-OneHalfCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
+              </div>
+            </div>
+          </div>
+          <div class="card half-card border consonant-Card consonant-OneHalfCard">
+            <div class="consonant-OneHalfCard-img" style="background-image: url(&quot;https://main--milo--adobecom.hlx.page/drafts/rclayton/media_138263efad88faf6be71169bc1fc66ee2dfb96661.jpeg?width=750&amp;format=jpeg&amp;optimize=medium&quot;);">
+            </div>
+            <div class="consonant-OneHalfCard-inner">
+              <div data-valign="middle">  
+                <h3 id="lorem-ipsum-dolor-sit-amet-3" class="consonant-OneHalfCard-title">Lorem ipsum dolor sit amet</h3>
+                <p class="consonant-OneHalfCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
+              </div>
               
             </div>
-          <div class="consonant-CardFooter"><div class="consonant-CardFooter-row" data-cells="2"><div class="consonant-CardFooter-cell consonant-CardFooter-cell--left"><a href="https://business.adobe.com/" class="con-button outline">Sign up</a></div><div class="consonant-CardFooter-cell consonant-CardFooter-cell--right"><a href="https://business.adobe.com/" class="con-button blue">Learn more</a></div></div></div></div></div><div class="card half-card border consonant-Card consonant-OneHalfCard">
-          
-        <div class="consonant-OneHalfCard-img" style="background-image: url(&quot;https://main--milo--adobecom.hlx.page/drafts/rclayton/media_138263efad88faf6be71169bc1fc66ee2dfb96661.jpeg?width=750&amp;format=jpeg&amp;optimize=medium&quot;);"></div><div class="consonant-OneHalfCard-inner">
-            <div data-valign="middle">
-              
-              <h3 id="lorem-ipsum-dolor-sit-amet-1" class="consonant-OneHalfCard-title">Lorem ipsum dolor sit amet</h3>
-              <p class="consonant-OneHalfCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
-              
+          </div>
+          <div class="card half-card border consonant-Card consonant-OneHalfCard">
+            <div class="consonant-OneHalfCard-img" style="background-image: url(&quot;https://main--milo--adobecom.hlx.page/drafts/rclayton/media_138263efad88faf6be71169bc1fc66ee2dfb96661.jpeg?width=750&amp;format=jpeg&amp;optimize=medium&quot;);">
             </div>
-          <div class="consonant-CardFooter"><div class="consonant-CardFooter-row" data-cells="2"><div class="consonant-CardFooter-cell consonant-CardFooter-cell--left"><a href="https://business.adobe.com/" class="con-button outline">Sign up</a></div><div class="consonant-CardFooter-cell consonant-CardFooter-cell--right"><a href="https://business.adobe.com/" class="con-button blue">Learn more</a></div></div></div></div></div><div class="card half-card border consonant-Card consonant-OneHalfCard">
-          
-        <div class="consonant-OneHalfCard-img" style="background-image: url(&quot;https://main--milo--adobecom.hlx.page/drafts/rclayton/media_138263efad88faf6be71169bc1fc66ee2dfb96661.jpeg?width=750&amp;format=jpeg&amp;optimize=medium&quot;);"></div><div class="consonant-OneHalfCard-inner">
-            <div data-valign="middle">
-              
-              <h3 id="lorem-ipsum-dolor-sit-amet-2" class="consonant-OneHalfCard-title">Lorem ipsum dolor sit amet</h3>
-              <p class="consonant-OneHalfCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
-              
+            <div class="consonant-OneHalfCard-inner">
+              <div data-valign="middle">  
+                <h3 id="lorem-ipsum-dolor-sit-amet-3" class="consonant-OneHalfCard-title">Lorem ipsum dolor sit amet</h3>
+                <p class="consonant-OneHalfCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
+              </div>
             </div>
-          <div class="consonant-CardFooter"><div class="consonant-CardFooter-row" data-cells="2"><div class="consonant-CardFooter-cell consonant-CardFooter-cell--left"><a href="https://business.adobe.com/" class="con-button outline">Sign up</a></div><div class="consonant-CardFooter-cell consonant-CardFooter-cell--right"><a href="https://business.adobe.com/" class="con-button blue">Learn more</a></div></div></div></div></div><div class="card half-card border consonant-Card consonant-OneHalfCard">
-          
-        <div class="consonant-OneHalfCard-img" style="background-image: url(&quot;https://main--milo--adobecom.hlx.page/drafts/rclayton/media_138263efad88faf6be71169bc1fc66ee2dfb96661.jpeg?width=750&amp;format=jpeg&amp;optimize=medium&quot;);"></div><div class="consonant-OneHalfCard-inner">
-            <div data-valign="middle">
-              
-              <h3 id="lorem-ipsum-dolor-sit-amet-3" class="consonant-OneHalfCard-title">Lorem ipsum dolor sit amet</h3>
-              <p class="consonant-OneHalfCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
-              
+          </div>
+
+          <div class="card half-card border consonant-Card consonant-OneHalfCard">
+            <div class="consonant-OneHalfCard-img" style="background-image: url(&quot;https://main--milo--adobecom.hlx.page/drafts/rclayton/media_138263efad88faf6be71169bc1fc66ee2dfb96661.jpeg?width=750&amp;format=jpeg&amp;optimize=medium&quot;);">
             </div>
-          <div class="consonant-CardFooter"><div class="consonant-CardFooter-row" data-cells="2"><div class="consonant-CardFooter-cell consonant-CardFooter-cell--left"><a href="https://business.adobe.com/" class="con-button outline">Sign up</a></div><div class="consonant-CardFooter-cell consonant-CardFooter-cell--right"><a href="https://business.adobe.com/" class="con-button blue">Learn more</a></div></div></div></div></div></div></div></div></div>
-    `;
+            <div class="consonant-OneHalfCard-inner">
+              <div data-valign="middle">  
+                <h3 id="lorem-ipsum-dolor-sit-amet-3" class="consonant-OneHalfCard-title">Lorem ipsum dolor sit amet</h3>
+                <p class="consonant-OneHalfCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="card half-card border consonant-Card consonant-OneHalfCard">
+            <div class="consonant-OneHalfCard-img" style="background-image: url(&quot;https://main--milo--adobecom.hlx.page/drafts/rclayton/media_138263efad88faf6be71169bc1fc66ee2dfb96661.jpeg?width=750&amp;format=jpeg&amp;optimize=medium&quot;);">
+            </div>
+            <div class="consonant-OneHalfCard-inner">
+              <div data-valign="middle">  
+                <h3 id="lorem-ipsum-dolor-sit-amet-3" class="consonant-OneHalfCard-title">Lorem ipsum dolor sit amet</h3>
+                <p class="consonant-OneHalfCard-text">Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim.</p>
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
+  </div>
+`};
