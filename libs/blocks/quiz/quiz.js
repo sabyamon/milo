a6a9a7338446745829c9da7d43715c377a222cf6
Sabya <sabyasachi.exe@gmail.com>
2023-03-15 21:54:20 -0700

    Cleaned up a lot. Morte than initial version now

--- libs/blocks/quiz/quiz.js
+++ libs/blocks/quiz/quiz.js
@@ -37,107 +37,92 @@ export default function init(el) {
   const link = el.querySelector(':scope > div > div > a');
   window.milo = window.milo || {};
   window.milo.quizConfigPath = link.text.toLowerCase();
-  fetchQuestionsData();
+  getAllData();
 }
 
-// Get initial quiz data
-async function fetchQuestionsData(questionId) {
-  await fetch(window.milo.quizConfigPath + QUESTIONS_EP_NAME)
-    .then((resp) => {
-      if (!resp.ok) return {};
-      const json = resp.json();
-      collectQuestions(json, questionId);
-    })
-    .then(() => {
-      // Do nothing
+async function getAllData() {
+  try {
+    Promise.all([getQuestions(), getStrings(), getResult()]).then(function (
+      values
+    ) {
+      collectQuestions();
+      //collectStrings()
     });
+  } catch (ex) {
+    console.log('Error while fetching data : ', ex);
+  }
 }
 
-// Get initial quiz data
-async function fetchStringsData(el, questionIdentifier) {
-  fetch(window.milo.quizConfigPath + STRINGS_EP_NAME).then(
-    (stringsResponse) => {
-      // if (!stringsResponse.ok) return {};
-      const stringsData = stringsResponse.json();
-      collectStrings(el, stringsData, questionIdentifier);
-    }
-  );
+async function getQuestions() {
+  const response = await fetch(window.milo.quizConfigPath + QUESTIONS_EP_NAME);
+  const qData = await response.json();
+  window.milo = window.milo || {};
+  window.milo.questions = qData;
+  questionsData = qData;
+  return qData;
 }
 
-const collectStrings = (el, stringsDataPromise, questionIdentifier) => {
-  console.log(
-    'questionIdentifier while collecting strings:',
-    questionIdentifier
+async function getStrings() {
+  const response = await fetch(window.milo.quizConfigPath + STRINGS_EP_NAME);
+  const sData = await response.json();
+  window.milo = window.milo || {};
+  window.milo.strings = sData;
+  stringsData = sData;
+  return sData;
+}
+
+async function getResult() {
+  const response = await fetch(window.milo.quizConfigPath + RESULTS_EP_NAME);
+  const rData = await response.json();
+  window.milo = window.milo || {};
+  window.milo.results = rData;
+  resultsData = rData;
+  return rData;
+}
+
+const collectStrings = () => {
+  const object = stringsData.questions.data.find(
+    (o) => o.q === userJourney.value.currentQuestion.questions
   );
-  stringsDataPromise.then((data) => {
-    stringsData = data;
-    const object = data.questions.data.find(
-      (o) => o.q === userJourney.value.currentQuestion.questions
-    );
-
-    // console.log('finding strings data ::', object)
-    userJourney.value.currentQuestion.heading = object.heading;
-    userJourney.value.currentQuestion.subhead = object.subhead;
-
-    // let heading;
-    // let subheading;
-    // stringsData.questions.data.forEach(el => {
-    //   console.log('el is:', el);
-    //   if (el.q === questionIdentifier) {
-    //       heading = el.heading;
-    //       subheading = el.subhead;
-    //   }
-    // })
-
-    // console.log('global strings data', stringsData.questions.data)
-    decorateBlockForeground(el);
-  });
+  userJourney.value.currentQuestion.heading = object['heading'];
+  userJourney.value.currentQuestion.subhead = object['sub-head'];
+  userJourney.value.currentQuestion.btnText = object['btn'];
+  // userJourney.value.currentQuestion.maxSelections = object['max-selections'];
+  // userJourney.value.currentQuestion.minSelections = object['min-selections'];
+  decorateBlockForeground();
 };
 
 // Gather all questions
-const collectQuestions = (response, questionIdentifier) => {
+const collectQuestions = (questionIdentifier) => {
   // If there is no questions, the data is malformed.
-  // console.log('response in collectQuestions', typeof response);
+  if (questionIdentifier !== undefined) {
+    questionsData.questions.data.forEach((q) => {
+      if (q.questions === questionIdentifier.trim()) {
+        userJourney.value.currentQuestion = q;
+      }
+    });
+  } else {
+    userJourney.value.currentQuestion = questionsData.questions.data[0];
+  }
 
-  console.log('questionIdentifier is:', questionIdentifier);
-  // questionIdentifier = 'q-rather'
+  // Find the parent element  `quiz` and treat that as the source of truth.
+  // TODO: need better identifier of this element.
 
-  response.then((resp) => {
-    questionsData = resp;
-    if (!resp.questions) {
-      console.log('Malformed data. Questions must be defined');
-    }
+  rootElement.classList.add('quiz-container');
 
-    if (questionIdentifier !== undefined) {
-      resp.questions.data.forEach((q) => {
-        if (q.questions === questionIdentifier) {
-          userJourney.value.currentQuestion = q;
-        }
-      });
-    } else {
-      userJourney.value.currentQuestion = resp.questions.data[0];
-    }
-
-    // Find the parent element  `quiz` and treat that as the source of truth.
-    // TODO: need better identifier of this element.
-
-    rootElement.classList.add('quiz-container');
-
-    const children = rootElement.querySelectorAll(':scope > div');
-    if (children.length > 0) {
-      children[0].classList.add('background');
-      decorateBlockBg(children[0]);
-    }
-    fetchStringsData(rootElement, questionIdentifier);
-  });
+  const children = rootElement.querySelectorAll(':scope > div');
+  if (children.length > 0) {
+    children[0].classList.add('background');
+    decorateBlockBg(children[0]);
+  }
+  collectStrings();
 };
 
 const decorateBlockBg = (node) => {
-  console.log('u journey in decorating bg', userJourney.value.currentQuestion);
   const backgroundElemTpl = `<div data-valign="middle">
   <picture>
     <source type="image/webp" srcset="${userJourney.value.currentQuestion.background}" media="(min-width: 400px)">
-    <source type="image/webp" srcset="${userJourney.value.currentQuestion.background}" >
+    <source type="image/webp" srcset="${userJourney.value.currentQuestion.background}">
     <source type="image/png" srcset="${userJourney.value.currentQuestion.background}"  media="(min-width: 400px)">
     <img loading="eager" alt="" type="image/png" src="${userJourney.value.currentQuestion.background}"  width="750" height="375">
   </picture>
@@ -146,8 +131,7 @@ const decorateBlockBg = (node) => {
   node.innerHTML = backgroundElemTpl; // Adding the background image for the quiz block.
 };
 
-const decorateBlockForeground = (el) => {
-  // console.log('u journey while painting foreground: ', userJourney.value)
+const decorateBlockForeground = () => {
   const foregroundElemTpl = `<div class="foreground container">
   <div data-valign="middle" class="text" daa-lh="${userJourney.value.currentQuestion.heading}">
     <h2 id="heading-xl-marquee-standard-medium-left" class="heading-XL">
@@ -156,21 +140,16 @@ const decorateBlockForeground = (el) => {
     <p class="detail-L">${userJourney.value.currentQuestion.subhead}</p>
   </div>
 </div>`;
-  el.insertAdjacentHTML('beforeend', foregroundElemTpl); // Adding foreground elements.
-
-  // Lets create the options.
-  // Find optioons for the current questions.
-  findAndInsertOptions(el);
+  rootElement.insertAdjacentHTML('beforeend', foregroundElemTpl); // Adding foreground elements.
+  findAndInsertOptions();
 };
 
-const findAndInsertOptions = (el) => {
-  // console.log('questionsData while creating options : ', JSON.parse(JSON.stringify(questionsData)));
-  const hamaraData = JSON.parse(JSON.stringify(questionsData));
+const findAndInsertOptions = () => {
   const currQuestion = userJourney.value.currentQuestion.questions;
-  const currentQuestionOptions = hamaraData[currQuestion].data;
-  console.log('currentQuestionOptions :: ', currentQuestionOptions);
-
-  el.insertAdjacentHTML('beforeend', quizOption(currentQuestionOptions)); // Adding quiz options.
+  rootElement.insertAdjacentHTML(
+    'beforeend',
+    getQuizOption(window.milo.strings[currQuestion].data)
+  ); // Adding quiz options.
 
   const quizOptionElems = document.querySelectorAll(
     '.quiz-options-container > div'
@@ -182,8 +161,6 @@ const findAndInsertOptions = (el) => {
 
   const nextBtn = document.querySelectorAll('.quiz-btn')[0];
   nextBtn.addEventListener('click', (event) => handleNext(event));
-
-  console.log('Lets see the user journey : ', userJourney.value);
 };
 
 const handleOptionSelection = (event) => {
@@ -208,7 +185,11 @@ const handleOptionSelection = (event) => {
   } else {
     currentSelections.push(clickedCardName); // Add selections data to an array
     clickedCard.classList.add('selected');
-    if (currentSelections.length === maxSelections) {
+    console.log('show me user journey ??', userJourney.value)
+    console.log('how many selected ??', currentSelections.length)
+    console.log('whats the max selection ??', parseInt(userJourney.value.currentQuestion['max-selections']))
+    console.log('whats the min selection ??', parseInt(userJourney.value.currentQuestion['min-selections']))
+    if (currentSelections.length === parseInt(userJourney.value.currentQuestion['max-selections'])) {
       // Find all the cards which do not have `selected` class in it and add a disabled option to it.
       const allCards = document.querySelectorAll('.quiz-option');
       allCards.forEach((card) => {
@@ -220,13 +201,13 @@ const handleOptionSelection = (event) => {
   }
 
   const quizNextBtn = document.querySelectorAll('.quiz-btn')[0];
-  if (currentSelections.length >= minSelections) {
+  if (currentSelections.length >= parseInt(userJourney.value.currentQuestion['min-selections'])) {
     quizNextBtn.removeAttribute('disabled');
   } else {
     quizNextBtn.setAttribute('disabled', 'true');
   }
 
-  console.log('currentSelections are : ', currentSelections);
+  // console.log('currentSelections are : ', currentSelections);
 };
 
 /**
@@ -236,64 +217,109 @@ const handleNext = () => {
   // Reset maxSelections
   // Reset minSelections
   // Reset currentSelections
-  console.log('clicked next');
+  // console.log('clicked next');
   // find the next paths by popping elements from currentSelections.
   // Lets create the next array.
+
+  console.log(
+    'current selections while creating the next array:',
+    currentSelections
+  );
+
   currentSelections.forEach((selection) => {
     // for each elem in current selection, find its coresponding next element and push it to the next array.
     const currQuestion = userJourney.value.currentQuestion.questions;
-    const nextElemArr = questionsData[currQuestion].data;
-    nextElemArr.forEach((nextItem) => {
+    const nextItems = questionsData[currQuestion].data;
+    nextItems.forEach((nextItem) => {
       if (nextItem.options === selection) {
+        // console.log('matched nextItem.options', nextItem)
+        // console.log('with selection', selection)
         const nextArr = nextItem.next;
         const arr = nextArr.split(',');
         arr.forEach((elem) => {
+          console.log('Element we are trying to add now', elem);
+          console.log('how does the next array look? ', next);
+          console.log('Does array include the elem?', next.includes(elem));
           if (!next.includes(elem)) {
-            next.push(elem);
+            if (elem === 'RESET') {
+              // if we encounter `RESET`, reset the next array and add new elements.
+              next = [];
+            }
+            if (elem !== 'RESET') {
+              // Add next element when its not `RESET`. TODO: combine these two logics.
+              console.log('Added to next array: ', elem);
+              next.push(elem);
+            }
           }
+          console.log('next array after each try to add', next);
         });
       }
     });
 
-    console.log('current question is :', currQuestion);
-    console.log('next elem for curr question is: ', next);
+    //     currentSelections = []; // It was here.. It was getting reset when two options were chosen.
 
-    // Call fetchQuestionsData(n) and remove that element from next array.
-    const shifted = next.shift();
-    console.log('calling flow for :: ', shifted);
-    // rootElement.innerHTML = ''; // values were not updating. Hence cleared out the data. TODO: Need to check again
-    const foregroundEl = rootElement.querySelectorAll('.foreground')[0].remove();
-    const optionsContainer = rootElement.querySelectorAll('.milo-card-wrapper')[0].remove();
-    fetchQuestionsData(shifted);
+    popElementAndExecuteFlow(next);
   });
+};
+
+const popElementAndExecuteFlow = (next) => {
+  currentSelections = [];
+  let shifted = next.shift();
+
+  if (shifted === 'RESULT') {
+    next.push('RESULT');
+    shifted = next.shift();
+  }
+
+  // console.log('calling flow for :: ', shifted);
+  // rootElement.innerHTML = ''; // values were not updating. Hence cleared out the data. TODO: Need to check again
+  rootElement.querySelectorAll('.foreground')[0].remove();
+  rootElement.querySelectorAll('.milo-card-wrapper')[0].remove();
+
+  if (shifted === 'RESULT') {
+    handleResultFlow();
+  } else {
+    collectQuestions(shifted);
+  }
+};
+
+/**
+ * Handling the result flow from here. Will need to make sure we capture all the data so that we can come back.
+ *
+ */
+const handleResultFlow = () => {
+  console.log('We are at the end of the flow! Route to result page');
+  // window.location.href = 'https://www.adobe.com';
 };
 
 const createOption = (optionsData) => {
   let domElem = '';
   for (let index in optionsData) {
     const cardData = optionsData[index];
+    const coverImage = cardData.cover;
+    const cardIcon = cardData.image;
     domElem =
       domElem +
-      `<div class="card half-card border consonant-Card consonant-OneHalfCard quiz-option" data-card-name="${cardData.options}">
-              <div class="consonant-OneHalfCard-image">
-                <img src="https://www.adobe.com/content/dam/cc/Images/app-recommender/multi-select/2-Video%20ICON.svg" alt="Video" class="icon">
-              </div>
-              <div class="consonant-OneHalfCard-inner">
-                <div data-valign="middle">  
-                  <h3 id="lorem-ipsum-dolor-sit-amet-3" class="consonant-OneHalfCard-title">${cardData.options}</h3>
-                  <p class="consonant-OneHalfCard-text">Edit or organize</p>
-                </div>
-              </div>
-            </div>`;
+        `<div class="card half-card border consonant-Card consonant-OneHalfCard quiz-option" data-card-name="${cardData.options}">
+          ${cardIcon ? `<div class="consonant-OneHalfCard-image"><img src="${cardIcon}" alt="Video" class="icon"></div>` : ''}
+          ${coverImage ? `<div class="consonant-OneHalfCard-img" style="background-image: url('${coverImage}');"></div>` : ''}
+          <div class="consonant-OneHalfCard-inner">
+            <div data-valign="middle">  
+              <h3 id="lorem-ipsum-dolor-sit-amet-3" class="consonant-OneHalfCard-title">${
+                cardData.title
+              }</h3>
+              <p class="consonant-OneHalfCard-text">${cardData.text}</p>
+            </div>
+          </div>
+        </div>`;
   }
 
   return domElem;
 };
 
-export const quizOption = (currentQuestionOptions) => {
+export const getQuizOption = (currentQuestionOptions) => {
   let option = '';
   option = createOption(currentQuestionOptions);
-  console.log('option is : ', option);
   return `
     <div class="milo-card-wrapper consonant-Wrapper consonant-Wrapper--1200MaxWidth">
       <div class="consonant-Wrapper-inner">
@@ -304,7 +330,7 @@ export const quizOption = (currentQuestionOptions) => {
           </div>
           <div class="quiz-button-container">
             <button disabled="true" aria-label="Next" data-quiz-button="" class="spectrum-Button spectrum-Button--outline spectrum-Button--sizeXL quiz-btn" daa-ll="Filters|cc:app-reco|Q#1/Photo">
-              <span class="quiz-Button-label">Next</span>
+              <span class="quiz-Button-label">${userJourney.value.currentQuestion.btnText}</span>
             </button>
           </div>
         </div>
